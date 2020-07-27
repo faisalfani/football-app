cd7ed5ca8b1652ead3b5a96097b8f9a2e460b9d4
faisalfani <177006047@student.unsil.ac.id>
2020-07-27 10:38:45 +0700

    rev1

--- sw.js
+++ sw.js
@@ -5,30 +5,30 @@ importScripts(
 if (workbox) console.log(`Workbox berhasil dimuat`);
 else console.log(`Workbox gagal dimuat`);
 
-workbox.precaching.precacheAndRoute([
-  { url: "/", revision: "1" },
-  { url: "/index.html", revision: "1" },
-  { url: "/nav.html", revision: "1" },
-  { url: "/css/style.css", revision: "1" },
-  { url: "/css/materialize.min.css", revision: "1" },
-  { url: "/js/materialize.min.js", revision: "1" },
-  { url: "/js/api.js", revision: "1" },
-  { url: "/js/nav.js", revision: "1" },
-  { url: "/js/main.js", revision: "1" },
-  { url: "/js/idb.js", revision: "1" },
-  { url: "/js/db.js", revision: "1" },
-  { url: "/manifest.json", revision: "1" },
-  { url: "/img/navicon.png", revision: "1" },
-  { url: "/img/icons/icon-72x72.png", revision: "1" },
-  { url: "/img/icons/icon-96x96.png", revision: "1" },
-  { url: "/img/icons/icon-152x152.png", revision: "1" },
-  { url: "/img/icons/icon-192x192.png", revision: "1" },
-  { url: "/img/icons/icon-384x384.png", revision: "1" },
-  { url: "/img/icons/icon-512x512.png", revision: "1" },
-  { url: "/pages/favTeam.html", revision: "1" },
-  { url: "/pages/standings.html", revision: "1" },
-  { url: "/pages/HOF.html", revision: "1" },
-]);
+workbox.precaching.precacheAndRoute(
+  [
+    { url: "/", revision: "1" },
+    { url: "/index.html", revision: "1" },
+    { url: "/nav.html", revision: "1" },
+    { url: "/css/style.css", revision: "1" },
+    { url: "/css/materialize.min.css", revision: "1" },
+    { url: "/js/materialize.min.js", revision: "1" },
+    { url: "/js/api.js", revision: "1" },
+    { url: "/js/nav.js", revision: "1" },
+    { url: "/js/main.js", revision: "1" },
+    { url: "/js/idb.js", revision: "1" },
+    { url: "/js/db.js", revision: "1" },
+    { url: "/manifest.json", revision: "1" },
+    { url: "/img/navicon.png", revision: "1" },
+    { url: "/img/icons/icon-72x72.png", revision: "1" },
+    { url: "/img/icons/icon-96x96.png", revision: "1" },
+    { url: "/img/icons/icon-152x152.png", revision: "1" },
+    { url: "/img/icons/icon-192x192.png", revision: "1" },
+    { url: "/img/icons/icon-384x384.png", revision: "1" },
+    { url: "/img/icons/icon-512x512.png", revision: "1" },
+  ],
+  { ignoreUrlParametersMatching: [/.*/] }
+);
 
 workbox.routing.registerRoute(
   new RegExp("/pages/"),
@@ -38,7 +38,7 @@ workbox.routing.registerRoute(
 );
 
 workbox.routing.registerRoute(
-  /^https:\/\/api\.football-data\.org\/v2\/competitions\/2021\//,
+  /^https:\/\/api\.football-data\.org\/v2\//,
   workbox.strategies.staleWhileRevalidate({
     cacheName: "apis",
   })
